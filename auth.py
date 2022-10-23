7772b9da4bbaffc4e893da082c5c1def578906a2
Soaring-Darkness <tsaturov-artur2004@mail.ru>
2022-10-23 19:50:32 +0300

    test

--- auth.py
+++ auth.py
@@ -9,7 +9,7 @@ def auth():
 
         # TODO: Добавить проверки.
         if not unique:
-            flash('Пожалуйста, введите свой уникальный код', category='warning')
+            flash('Пожалуйста, введите свой уникальный код пж пж пж', category='warning')
             return render_template('auth.html')
 
         if unique and (6 > len(unique) < 8):
