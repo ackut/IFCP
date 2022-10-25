83e490f96d51ddcbafb0da856bf959dfeee00c70
ackut <askit.lab@gmail.com>
2022-10-25 17:18:55 +0300

    Почистил фсё.

--- auth.py
+++ auth.py
@@ -4,16 +4,4 @@ from flask import Flask, render_template, url_for, request, flash
 
 @app.route('/auth/', methods=['POST', 'GET'])
 def auth():
-    if request.method == 'POST':
-        unique = request.form['unique']
-
-        # TODO: Добавить проверки.
-        if not unique:
-            flash('Пожалуйста, введите свой уникальный код', category='warning')
-            return render_template('auth.html')
-
-        if unique and (6 > len(unique) < 8):
-            flash('Длина', category='warning')
-
-
     return render_template('auth.html')
--- panel.py
+++
@@ -1,7 +0,0 @@
-from app import app
-from flask import Flask, render_template, url_for, request, flash
-
-
-@app.route('/panel/', methods=['POST', 'GET'])
-def panel():
-    return render_template('panel.html')
